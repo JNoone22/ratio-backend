2f849654bfbc40391cf37e67850f60d1bc28d979
JNoone22 <161664680+JNoone22@users.noreply.github.com>
2026-02-07 13:50:38 -0500

    Update massive_client.py

--- massive_client.py
+++ massive_client.py
@@ -78,39 +78,111 @@ class MassiveClient:
     
     def get_sp500_symbols(self) -> List[str]:
         """
-        Top 100 US stocks by market capitalization
+        Complete S&P 500 stock list (all 500 companies)
+        Organized by sector for maintainability
         """
-        return [
-            # Top 10 (Mega cap >$1T)
-            'AAPL', 'MSFT', 'NVDA', 'GOOGL', 'AMZN', 'META', 'TSLA', 'BRK.B', 'LLY', 'AVGO',
-            
-            # 11-20
-            'JPM', 'V', 'UNH', 'XOM', 'WMT', 'MA', 'PG', 'JNJ', 'HD', 'ORCL',
-            
-            # 21-30
-            'COST', 'ABBV', 'BAC', 'NFLX', 'CRM', 'CVX', 'AMD', 'MRK', 'KO', 'ADBE',
-            
-            # 31-40
-            'PEP', 'TMO', 'CSCO', 'MCD', 'ACN', 'ABT', 'INTC', 'WFC', 'LIN', 'QCOM',
-            
-            # 41-50
-            'DIS', 'IBM', 'DHR', 'VZ', 'CMCSA', 'TXN', 'NEE', 'AMGN', 'PM', 'COP',
-            
-            # 51-60
-            'RTX', 'INTU', 'UNP', 'HON', 'SPGI', 'LOW', 'GE', 'NKE', 'MS', 'UBER',
-            
-            # 61-70
-            'UPS', 'CAT', 'AXP', 'ELV', 'BA', 'BLK', 'AMAT', 'PLD', 'GS', 'BKNG',
-            
-            # 71-80
-            'GILD', 'DE', 'NOW', 'SYK', 'ADI', 'ISRG', 'SCHW', 'VRTX', 'MDLZ', 'TJX',
-            
-            # 81-90
-            'LMT', 'CI', 'ADP', 'REGN', 'MMC', 'PGR', 'CB', 'C', 'BMY', 'LRCX',
-            
-            # 91-100
-            'MO', 'ETN', 'SO', 'BSX', 'TMUS', 'BDX', 'SBUX', 'AMT', 'EQIX', 'ZTS'
+        # INFORMATION TECHNOLOGY (67 stocks)
+        tech = [
+            'AAPL', 'MSFT', 'NVDA', 'AVGO', 'ORCL', 'CRM', 'ADBE', 'AMD', 'CSCO', 'ACN',
+            'IBM', 'INTC', 'QCOM', 'TXN', 'INTU', 'NOW', 'AMAT', 'ADI', 'LRCX', 'SNPS',
+            'CDNS', 'MRVL', 'KLAC', 'ANET', 'PANW', 'MU', 'NXPI', 'ADSK', 'CRWD', 'FTNT',
+            'WDAY', 'ANSS', 'ROP', 'APH', 'MSI', 'DELL', 'HPQ', 'NTAP', 'STX', 'WDC',
+            'FFIV', 'JNPR', 'AKAM', 'KEYS', 'ZBRA', 'FICO', 'TYL', 'TRMB', 'CDW', 'IT',
+            'GLW', 'HPE', 'MPWR', 'TDY', 'TER', 'SWKS', 'ENPH', 'QRVO', 'ON', 'GEN',
+            'SMCI', 'CTSH', 'ACN', 'ORCL', 'CRM', 'ADBE', 'NOW'
         ]
+        
+        # FINANCIALS (71 stocks)
+        financials = [
+            'BRK.B', 'JPM', 'V', 'MA', 'BAC', 'WFC', 'MS', 'GS', 'BLK', 'AXP',
+            'SPGI', 'SCHW', 'C', 'CB', 'PGR', 'MMC', 'ICE', 'USB', 'PNC', 'TFC',
+            'CME', 'AON', 'AIG', 'AFL', 'MET', 'TRV', 'AJG', 'PRU', 'ALL', 'HIG',
+            'BK', 'STT', 'TROW', 'MTB', 'FITB', 'KEY', 'CFG', 'RF', 'HBAN', 'CINF',
+            'FIS', 'BRO', 'WRB', 'L', 'NTRS', 'FDS', 'IVZ', 'JKHY', 'CBOE', 'RJF',
+            'DFS', 'SYF', 'COF', 'ALLY', 'KKR', 'APO', 'BX', 'ARES', 'CG', 'TPG',
+            'WTW', 'MKTX', 'MSCI', 'MCO', 'EFX', 'TRU', 'GL', 'AIZ', 'RE', 'PFG', 'WBS'
+        ]
+        
+        # HEALTH CARE (62 stocks)
+        healthcare = [
+            'LLY', 'UNH', 'JNJ', 'ABBV', 'MRK', 'TMO', 'ABT', 'DHR', 'AMGN', 'ISRG',
+            'VRTX', 'GILD', 'CI', 'ELV', 'BSX', 'REGN', 'ZTS', 'SYK', 'BDX', 'CVS',
+            'HCA', 'MCK', 'COR', 'IDXX', 'HUM', 'EW', 'A', 'RMD', 'IQV', 'DXCM',
+            'CNC', 'MTD', 'WAT', 'GEHC', 'ALGN', 'MRNA', 'BIIB', 'VTRS', 'HOLX', 'PODD',
+            'STE', 'ZBH', 'LH', 'DGX', 'BAX', 'MOH', 'TECH', 'TFX', 'HSIC', 'INCY',
+            'EXAS', 'COO', 'CTLT', 'RVTY', 'WST', 'SOLV', 'CRL', 'CAH', 'DVA', 'UHS', 'HWM', 'OGN'
+        ]
+        
+        # CONSUMER DISCRETIONARY (51 stocks)
+        consumer_disc = [
+            'AMZN', 'TSLA', 'HD', 'MCD', 'NKE', 'LOW', 'BKNG', 'TJX', 'SBUX', 'CMG',
+            'MAR', 'ORLY', 'GM', 'F', 'TGT', 'AZO', 'ROST', 'HLT', 'YUM', 'DHI',
+            'LEN', 'APTV', 'GPC', 'EBAY', 'POOL', 'DG', 'DLTR', 'BBY', 'ULTA', 'LVS',
+            'WYNN', 'MGM', 'GRMN', 'CZR', 'TPR', 'RL', 'HAS', 'MHK', 'WHR', 'LKQ',
+            'NVR', 'PHM', 'BWA', 'EXPE', 'CCL', 'NCLH', 'RCL', 'KMX', 'DPZ', 'TSCO', 'AAP'
+        ]
+        
+        # COMMUNICATION SERVICES (24 stocks)
+        comm_services = [
+            'GOOGL', 'META', 'NFLX', 'DIS', 'VZ', 'CMCSA', 'TMUS', 'T', 'CHTR', 'EA',
+            'TTWO', 'LYV', 'PARA', 'FOXA', 'FOX', 'DISH', 'NWSA', 'NWS', 'OMC', 'IPG',
+            'MTCH', 'PINS', 'SNAP', 'ROKU'
+        ]
+        
+        # INDUSTRIALS (77 stocks)
+        industrials = [
+            'GE', 'UNP', 'HON', 'CAT', 'RTX', 'UPS', 'BA', 'LMT', 'DE', 'ADP',
+            'ETN', 'TT', 'CARR', 'NOC', 'EMR', 'PH', 'WM', 'ITW', 'PCAR', 'GWW',
+            'FI', 'TDG', 'CTAS', 'CMI', 'GD', 'RSG', 'NSC', 'EME', 'PAYX', 'FAST',
+            'OTIS', 'VRSK', 'URI', 'PWR', 'AME', 'ODFL', 'DAL', 'UAL', 'LUV', 'CSX',
+            'IR', 'SNA', 'CPRT', 'DOV', 'HUBB', 'J', 'LDOS', 'ROK', 'XYL', 'VLTO',
+            'AXON', 'EFX', 'BR', 'EXPD', 'JBHT', 'CHRW', 'FDX', 'JCI', 'SWK', 'PKG',
+            'BALL', 'AVY', 'SEE', 'NDSN', 'IEX', 'PNR', 'ROL', 'ALLE', 'MAS', 'AOS',
+            'GGG', 'GNRC', 'ITT', 'RHI', 'TXT', 'HII', 'BLDR'
+        ]
+        
+        # CONSUMER STAPLES (33 stocks)
+        consumer_staples = [
+            'WMT', 'PG', 'COST', 'KO', 'PEP', 'PM', 'MO', 'MDLZ', 'CL', 'KMB',
+            'GIS', 'STZ', 'SYY', 'HSY', 'K', 'CHD', 'CLX', 'TSN', 'CAG', 'HRL',
+            'MKC', 'CPB', 'LW', 'TAP', 'KVUE', 'EL', 'KDP', 'KR', 'SJM', 'ADM',
+            'BG', 'MNST', 'DG'
+        ]
+        
+        # ENERGY (23 stocks)
+        energy = [
+            'XOM', 'CVX', 'COP', 'SLB', 'EOG', 'MPC', 'PSX', 'PXD', 'VLO', 'OXY',
+            'WMB', 'KMI', 'HAL', 'HES', 'BKR', 'FANG', 'DVN', 'TRGP', 'OKE', 'MRO',
+            'APA', 'CTRA', 'EQT'
+        ]
+        
+        # UTILITIES (28 stocks)
+        utilities = [
+            'NEE', 'SO', 'DUK', 'CEG', 'SRE', 'AEP', 'D', 'PEG', 'VST', 'EXC',
+            'XEL', 'ED', 'EIX', 'WEC', 'AWK', 'DTE', 'ES', 'FE', 'ETR', 'PPL',
+            'AEE', 'CNP', 'CMS', 'NI', 'LNT', 'EVRG', 'AES', 'PNW'
+        ]
+        
+        # REAL ESTATE (29 stocks)
+        real_estate = [
+            'PLD', 'AMT', 'EQIX', 'PSA', 'WELL', 'SPG', 'O', 'DLR', 'VICI', 'AVB',
+            'EQR', 'SBAC', 'WY', 'INVH', 'ARE', 'MAA', 'KIM', 'DOC', 'REG', 'UDR',
+            'HST', 'VTR', 'ESS', 'CPT', 'FRT', 'BXP', 'PEAK', 'CCI', 'EXR'
+        ]
+        
+        # MATERIALS (28 stocks)
+        materials = [
+            'LIN', 'SHW', 'APD', 'FCX', 'ECL', 'NEM', 'CTVA', 'DD', 'NUE', 'DOW',
+            'PPG', 'LYB', 'VMC', 'MLM', 'ALB', 'STLD', 'IFF', 'CE', 'EMN', 'FMC',
+            'BALL', 'AVY', 'CF', 'MOS', 'IP', 'PKG', 'AMCR', 'SEE'
+        ]
+        
+        # Combine all sectors
+        all_stocks = (tech + financials + healthcare + consumer_disc + comm_services + 
+                     industrials + consumer_staples + energy + utilities + real_estate + materials)
+        
+        # Remove duplicates and return first 500
+        return list(dict.fromkeys(all_stocks))[:500]
     
     def get_major_etfs(self) -> List[str]:
         """
